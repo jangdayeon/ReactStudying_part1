331d809c800604c485f2e758f0d3aeb6e04f6637
jangdayeon <hyejmh@naver.com>
2023-07-18 21:49:48 +0900

    input 1 : 사용자가 입력한 글 다루기 & input 다루기 2 : 블로그 글발행 기능 만들기

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
   let [따봉, 따봉변경] = useState([0,0,0]);
   let [modal, setModal] = useState('닫힘'); 
   let [title,setTitle] = useState(0);
+  let [입력값,입력값변경]= useState('');
   
 
   function 함수(){
@@ -31,31 +32,39 @@ function App() {
         return(
           <div className="list" key={i}>
         <h4 onClick={()=>{
-          ////////////////////////////////////////////글제목을 누를 때마다 title이 재설정 되도록!
           setTitle(i); 
-          ////////////////////////////////////////////
+          
           modal =='열림'? setModal('닫힘'):setModal('열림');
-          }}>{글데이터}  <span onClick={()=>{ let copy = [...따봉]; copy[i]+=1; 따봉변경(copy)}}>❤️</span> {따봉[i]} </h4> 
+          }}>{글데이터}  
+          {/* ///////////////////////////////////////////// */}
+          <span onClick={(e)=>{ e.stopPropagation(); let copy = [...따봉]; copy[i]+=1; 따봉변경(copy)}}>❤️</span> {따봉[i]} 
+          <span> <button onClick={(e)=>{e.stopPropagation(); let copy = [...a]; copy.splice(i,1); 제목변경(copy);}}>삭제버튼</button></span>
+          {/* ///////////////////////////////////////////// */}
+          </h4> 
         {/* <h4>{a[i]} </h4> */}
         <p>2월 17일 발행</p>
         </div>
         )
       })}
+      {/* //////////////////////////////////////////////// */}
+      <input onChange={(e)=>{입력값변경(e.target.value); console.log(입력값);}}/>
+      <button onClick={()=>{let copy = [...a]; copy.unshift(입력값); 제목변경(copy);}}>버튼</button>
+      {/* //////////////////////////////////////////////// */}
+
       {
-        /////////////////////////////////////////////
         modal == '열림'? <Modal 글제목={a} color={'skyblue'} title={title}/> : '아님'
-        /////////////////////////////////////////////
+
       }
 
       <div>
         <h1 style={{color:'red'}}>총 정리</h1>
-        modal의 제목이 글제목을 누를 때마다 변경된 값이 출력되도록 하는 방법을 다루었다.<br></br>
+        다양한 이벤트 핸들러에 대해서 알고 있어라!<br></br>
+        이벤트 버블링 : 클릭이벤트는 상위 html로 퍼짐<br></br>
+        상위html로 퍼지는 이벤트버블링을 막고싶으면 e.stopPropagation(); (파라미터는 보통 e를 사용!)
         <h3>
-          useState를 이용해서 새로운 변수를 만들고, 그 변수의 값이 글제목을 누를 때마다 변경하게 만듦.<br></br>
-          그리고 그 변경된 값을 자식함수로 넘겨주면 된다.<br></br>
+          <br></br>
         </h3>
         <h4>
-          (중요) state 만드는 곳은 state 사용하는 컴포넌트들 중 최상위 컴포넌트
         </h4>
         <hr style={{width:'100px', height:'5px',backgroundColor:'black'}}></hr>
         
@@ -66,7 +75,7 @@ function App() {
   
 
 }
-////////////////////////////////////////////////////////////
+
 function Modal(props){
   return ( 
     <div className="modal" style={{background : props.color}}>
@@ -77,6 +86,5 @@ function Modal(props){
       </div>
   )
 }
-////////////////////////////////////////////////////////////
 
 export default App;
